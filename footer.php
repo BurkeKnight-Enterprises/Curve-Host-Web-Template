8dc2df794f0a8b5df082724224391c14e9dd6654
Burke Knight <github@burkeknight.com>
2015-05-30 21:35:28 -0400

    Updated footer.php

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
-	<div id="bottom"><p>Copyright © <?php echo date("Y");?> - <? echo "$yourdomain" ;?>. All Rights Reserved<br />
+﻿	<div id="bottom"><p>Copyright © <?php echo date("Y");?> - <? echo "$yourdomain" ;?>. All Rights Reserved<br />
 	<!-- The next line MUST NOT be removed -->
-	Curve-Host Template by <a href="http://www.burkeknight.com/" target="_blank">BurkeKnight</a> & <a href="http://htmlstig.com/" target="_blank">HTMLstig</a></p></div>
+	Curve-Host Template by <a href="http://www.burkeknight.org/" target="_blank">BK-Templates</a> & <a href="http://htmlstig.com/" target="_blank">HTMLstig</a></p></div>
 	<div id="footer">
 	</div>
 <div id="navigation_up_down_4137"><noscript>Javascript is disable - <a href="http://www.webestools.com/">http://www.webestools.com/</a> - <a href="http://www.webestools.com/navigation-buttons-generator-scroll-top-bottom-shortcut-page-up-down-scroll-buttons-javascript.html">Navigation Buttons Generator</a></noscript><script type="text/javascript" src="http://services.webestools.com/navigation_up_down/4-4137-right.js"></script></div>
